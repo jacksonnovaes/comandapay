4f1e94ccb2b5f6402ab869b5f40440b67229b6a4
Jackson Bispo <jackson.novaes@live.com>
2024-05-24 18:28:13 -0300

    edite items set

---
+++ establishment-ms/src/main/java/com/codexmind/establishment/converters/ItemOrderConverter.java
@@ -0,0 +1,79 @@
+package com.codexmind.establishment.converters;
+
+import com.codexmind.establishment.domain.ItemOrder;
+import com.codexmind.establishment.dto.ItemOrderRequestDTO;
+import com.codexmind.establishment.dto.ItemOrderResponseDTO;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ItemOrderConverter {
+    public static Set<ItemOrderResponseDTO> toDTO(Set<ItemOrder> itemOrderList) {
+        return itemOrderList.stream()
+                .map(itemOrder -> new ItemOrderResponseDTO(
+                        itemOrder.getId(),
+                        itemOrder.getProduct().getName(),
+                        itemOrder.getPrice(),
+                        itemOrder.getProduct().getMenu().getId(),
+                        itemOrder.getProduct().getId(),
+                        itemOrder.getOrder().getId(),
+                        itemOrder.getCustomerId(),
+                        itemOrder.getEmployeeId(),
+                        itemOrder.getEstablishmentId(),
+                        itemOrder.getStatus(),
+                        itemOrder.getTotalOrder(),
+                        itemOrder.getOpenInstant(),
+                        itemOrder.getQuantity(),
+                        itemOrder.getTotalAmount(),
+                        itemOrder.getDiscount(),
+                        itemOrder.getUnitPrice()
+                ))
+                .collect(Collectors.toSet());
+    }
+
+    public static List<ItemOrderResponseDTO> toDTOSets(List<ItemOrder> itemOrderList) {
+        return itemOrderList.stream()
+                .map(itemOrder -> new ItemOrderResponseDTO(
+                        itemOrder.getId(),
+                        itemOrder.getProduct().getName(),
+                        itemOrder.getPrice(),
+                        itemOrder.getProduct().getMenu().getId(),
+                        itemOrder.getProduct().getId(),
+                        itemOrder.getOrder().getId(),
+                        itemOrder.getCustomerId(),
+                        itemOrder.getEmployeeId(),
+                        itemOrder.getEstablishmentId(),
+                        itemOrder.getStatus(),
+                        itemOrder.getTotalOrder(),
+                        itemOrder.getOpenInstant(),
+                        itemOrder.getQuantity(),
+                        itemOrder.getTotalAmount(),
+                        itemOrder.getDiscount(),
+                        itemOrder.getUnitPrice()
+                ))
+                .collect(Collectors.toList());
+    }
+
+    public  static ItemOrderResponseDTO toDTO(ItemOrder itemOrder){
+        return new ItemOrderResponseDTO(
+                itemOrder.getId(),
+                itemOrder.getProduct().getName(),
+                itemOrder.getPrice(),
+                itemOrder.getProduct().getMenu().getId(),
+                itemOrder.getProduct().getId(),
+                itemOrder.getOrder().getId(),
+                itemOrder.getCustomerId(),
+                itemOrder.getEmployeeId(),
+                itemOrder.getEstablishmentId(),
+                itemOrder.getStatus(),
+                itemOrder.getTotalOrder(),
+                itemOrder.getOpenInstant(),
+                itemOrder.getQuantity(),
+                itemOrder.getTotalAmount(),
+                itemOrder.getDiscount(),
+                itemOrder.getUnitPrice()
+        );
+    }
+
+}
---
+++ establishment-ms/src/main/java/com/codexmind/establishment/usecases/product/GetProductsByMenu.java
@@ -0,0 +1,22 @@
+package com.codexmind.establishment.usecases.product;
+
+
+import com.codexmind.establishment.domain.Product;
+import com.codexmind.establishment.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class GetProductsByMenu {
+
+    private final ProductRepository productRepository;
+
+    public Set<Product> execute(Integer id){
+
+        return productRepository.getProductdsByMenuandEstablishmente(id);
+    }
+}
