129dd14c6b7eae78005004918308a7dd6d8b4776
jacksonnovaes <jackson.novaes@live.com>
2025-03-09 13:22:08 -0300

    ci:update ci

--- start-me.sh
+++ start-me.sh
@@ -7,4 +7,4 @@ docker build -t establishment-ms -f establishment-ms/Dockerfile ./establishment-
 
 docker build -t pay-auth -f pay-auth/Dockerfile ./pay-auth
 
-docker compose compose --file docker-compose-dev.yaml up --build --force-recreate --remove-orphans
+docker compose --file docker-compose-dev.yaml up --build --force-recreate --remove-orphans
